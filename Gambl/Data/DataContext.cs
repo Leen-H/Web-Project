ac8637697fd851732d867f5b25a0869e569de0bd
Leen Husseini <leen.husseini05@gmail.com>
2024-12-10 21:28:49 +0300

    Gambl

---
+++ Gambl/Data/DataContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Gambl.Models;
+
+namespace Gambl.Data
+{
+    
+    public class  DataContext :DbContext{
+        public DataContext(DbContextOptions<DataContext>options):base(options)
+    {
+        
+    }
+        public DbSet<UserInfo> UserInfos=>Set<UserInfo>();
+        public DbSet<CourseInfo> CourseInfos=>Set<CourseInfo>();
+        public DbSet<InstructorInfo> InstructorInfos=>Set<InstructorInfo>();
+    }
+
+    
+}
---
+++ Gambl/Program.cs
@@ -0,0 +1,19 @@
+
+using Gambl.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DataContext>(options=>{
+    var connectionString = builder.Configuration.GetConnectionString("database");
+    options.UseSqlite(connectionString);
+});
+
+var app = builder.Build();
+
+app.UseStaticFiles(); 
+
+app.MapDefaultControllerRoute();
+
+app.Run();
