bdd09c9547c29a84e6b78b577285cda70e4f300e
Leen Husseini <leen.husseini05@gmail.com>
2024-12-13 18:08:30 +0300

    Gambl

---
+++ Gambl/Models/UserInfo.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Gambl.Models{
+    public class UserInfo{
+        [Key]
+        public int UserId {get;set;}
+        public string? UserFN { get; set; }
+        public string? UserLN { get; set; }
+        public string? UserEmail { get; set; }
+        public string? UserPhone { get; set; }
+
+    }
+
+}
+
