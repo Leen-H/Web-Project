17c3779dca95d39e2aa21b8a12a7b7d59a39b397
Betül çelik <betulcelik017@gmail.com>
2024-12-14 12:33:58 +0300

    Updated login page

--- Gambl/Controllers/LoginController.cs
+++ Gambl/Controllers/LoginController.cs
@@ -1,17 +1,59 @@
+
 using Microsoft.AspNetCore.Mvc;
+using Gambl.Data;
+using Gambl.Models;
 
 namespace Gambl.Controllers
 {
-    public class LoginController:Controller{
-        public IActionResult Index(){
-            return View();
+    public class LoginController : Controller
+    {
+        private readonly DataContext _context;
+
+        public LoginController(DataContext context)
+        {
+            _context = context;
         }
-        public IActionResult Admission(){
-            return View();
-        }
-        public IActionResult SignUp(){
+
+        
+        public IActionResult Index()
+        {
             return View();
         }
 
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Signup(UserInfo user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+       
+        public IActionResult Admission()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Admission(string email, string password)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.UserEmail == email && u.Password == password);
+            if (user != null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Message = "Invalid email or password.";
+            return View();
+        }
     }
 }
